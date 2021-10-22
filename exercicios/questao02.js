39d46620ebbdcba9541dd733588126b38cfc3d7a
Monalysa Yoshikawa <monalysa.yoshikawa@gruposbf.com.br>
2021-10-22 00:32:03 -0300

    inclusao do layout das outras questões

---
+++ exercicios/questao02.js
@@ -0,0 +1,5 @@
+let temperaturaF = document.getElementById("tempF"); //window.prompt("Temperatura em graus Fahrenheit", 0);
+let temperaturaC = 5 * ((temperaturaF -32) / 9)
+
+//console.log(`sua temperatura ${temperaturaC} `)
+console.log(temperaturaF, "e isso")
