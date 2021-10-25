a2ceaa5bc632b7569b395974514a3ac8726174d6
Monalysa Yoshikawa <monalysa.yoshikawa@gruposbf.com.br>
2021-10-25 00:02:58 -0300

    more exercises

--- exercicios/questao05.js
+++ exercicios/questao05.js
@@ -1,12 +1,17 @@
 // 5 – Faça um programa que verifique se uma letra digitada é vogal ou consoante.
 
-let vogais = ["a", "e", "i", "o", "u"]
-let letra = " "
+function letras () {
+    let vogais = ["a", "e", "i", "o", "u"]
+    let letra = document.getElementById("letra").value;
+    letra = letra.toLowerCase();
 
-if(letra == vogais){
-    return " É uma letra vogais"
 
-}else{
-    return " É uma letra consoante"
+    if (letra==='a' || letra==='e' || letra==='i' || letra==='o' || letra==='u') {
+        console.log("Resultado: A letra " + letra + " é uma vogal");
+        return document.getElementById("letraAdicional").innerHTML = "Resultado: A letra " + letra + " é uma vogal";
+      } else{
+        console.log("Resultado: A letra " + letra + " é uma consoante");
+        return document.getElementById("letraAdicional").innerHTML = "Resultado: A letra " + letra + " é uma consoante";
+      }
+
 }
-
