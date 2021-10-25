68a169e9a64a046fa61caa94ae88948c33c07879
Monalysa Yoshikawa <monalysa.yoshikawa@gruposbf.com.br>
2021-10-25 00:24:31 -0300

    more question ok

--- exercicios/questao06.js
+++ exercicios/questao06.js
@@ -1,3 +1,25 @@
 function boletim() {
-    
+
+    let studentFirstGrade01 = parseFloat(document.getElementById("studentFirstGrade01").value);
+    let studentSecondGrade01 = parseFloat(document.getElementById("studentSecondGrade01").value);
+    let studentFirstGrade02 = parseFloat(document.getElementById("studentFirstGrade02").value);
+    let studentSecondtGrade02 = parseFloat(document.getElementById("studentSecondtGrade02").value);
+
+    let mediaAluno01 = (studentFirstGrade01 + studentSecondGrade01)/2;
+    let mediaAluno02 = (studentFirstGrade02 + studentSecondtGrade02)/2;
+
+    if(mediaAluno01 === 10){
+        console.log(document.getElementById("resultMedia")
+        .innerHTML = "Resultado: A media do Aluno 01 foi " + mediaAluno01 + " parabens esta Aprovado!!!")
+
+    } else if(media>=7){
+        console.log(document.getElementById("resultMedia")
+      .innerHTML = "Resultado: A media do Aluno 01 foi " + mediaAluno01 + " parabens esta Aprovado na Media!!!")
+    }
+    else{
+        console.log(document.getElementById("resultMedia")
+        .innerHTML = "Resultado: A media do Aluno 01 foi " + mediaAluno01 + " e esta reprovado!!!")
+
+    }
+
 }
