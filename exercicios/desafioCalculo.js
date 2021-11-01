77e41b1720857f3b1e081414152d4127e9d17e87
Monalysa Yoshikawa <monalysa.yoshikawa@gruposbf.com.br>
2021-11-01 17:12:28 -0300

    inclusao do desafio de calculo

---
+++ exercicios/desafioCalculo.js
@@ -0,0 +1,6 @@
+function desafioCalculo () {
+    let number = getElementById("number").value
+    
+
+    document.getElementById("numberMath").innerHTML = "Resultado: O quadrado do numero é  " 
+}
